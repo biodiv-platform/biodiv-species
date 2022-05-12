dd2ec6edebb425af6b77f6c22888fb2f7c205615
teamdraftbox <teamdraftbox@gmail.com>
2022-05-12 17:49:06 +0530

    ⚡️ es update species bulk update

--- src/main/java/com/strandls/species/es/util/ESUpdate.java
+++ src/main/java/com/strandls/species/es/util/ESUpdate.java
@@ -2,6 +2,7 @@ package com.strandls.species.es.util;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -20,6 +21,8 @@ import com.strandls.esmodule.ApiException;
 import com.strandls.esmodule.controllers.EsServicesApi;
 import com.strandls.species.pojo.ShowSpeciesPage;
 import com.strandls.species.service.SpeciesServices;
+import com.strandls.userGroup.controller.UserGroupSerivceApi;
+import com.strandls.userGroup.pojo.UserGroupIbp;
 
 public class ESUpdate {
 
@@ -32,6 +35,9 @@ public class ESUpdate {
 	private SpeciesServices speciesService;
 
 	@Inject
+	private UserGroupSerivceApi ugService;
+
+	@Inject
 	private ObjectMapper om;
 
 	public void esBulkUpload(String speciesIds) {
@@ -41,43 +47,30 @@ public class ESUpdate {
 			return;
 		}
 
-		List<ShowSpeciesPage> ESSpeciesShowList = new ArrayList<ShowSpeciesPage>();
+		List<Map<String, Object>> ESSpeciesShowList = new ArrayList<>();
 
 		try {
 
 			for (String id : speciesIds.split(",")) {
-				ESSpeciesShowList.add(speciesService.showSpeciesPage(Long.parseLong(id)));
+				List<UserGroupIbp> userGroupList = new ArrayList<>();
+
+				try {
+					userGroupList = ugService.getSpeciesUserGroup(id);
+				} catch (com.strandls.userGroup.ApiException e) {
+					logger.error(e.getMessage());
+				}
+
+				Map<String, Object> payload = new HashMap<>();
+				payload.put("id", id);
+				payload.put("userGroups", userGroupList);
+				if (!userGroupList.isEmpty()) {
+					ESSpeciesShowList.add(payload);
+				}
 			}
 
 			if (!ESSpeciesShowList.isEmpty()) {
 
-				List<Map<String, Object>> bulkEsDoc = ESSpeciesShowList.stream().map(s -> {
-					SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-					om.setDateFormat(df);
-					@SuppressWarnings("unchecked")
-					String payload = null;
-					JsonNode rootNode = null;
-					
-					try {
-						payload = om.writeValueAsString(s);
-						rootNode = om.readTree(payload);
-					} catch (JsonProcessingException e) {
-						logger.error(e.getMessage());
-					}
-
-					if (s.getTaxonomyDefinition().getDefaultHierarchy() != null
-							&& !s.getTaxonomyDefinition().getDefaultHierarchy().isEmpty() && rootNode != null) {
-						JsonNode child = ((ObjectNode) rootNode).get("taxonomyDefinition");
-						((ObjectNode) child).replace("defaultHierarchy", null);
-					}
-					
-					
-					
-					return om.convertValue(rootNode,new TypeReference<Map<String, Object>>(){});
-
-				}).collect(Collectors.toList());
-
-				String json = om.writeValueAsString(bulkEsDoc);
+				String json = om.writeValueAsString(ESSpeciesShowList);
 
 				esService.bulkUpload(SpeciesIndex.INDEX.getValue(), SpeciesIndex.TYPE.getValue(), json.toString());
 
