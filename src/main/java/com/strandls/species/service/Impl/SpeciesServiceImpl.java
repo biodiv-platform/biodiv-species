19e7ee56e98306a02ade201ba7569c72a176987c
prakhar-s <prakhardevsingh4@gmail.com>
2025-08-07 15:24:06 +0530

    refactor: :recycle: changed activity description

--- src/main/java/com/strandls/species/service/Impl/SpeciesServiceImpl.java
+++ src/main/java/com/strandls/species/service/Impl/SpeciesServiceImpl.java
@@ -335,8 +335,8 @@ public class SpeciesServiceImpl implements SpeciesServices {
 
 			ESSpeciesUpdate(updatedSpecies.getId());
 
-			String activityDesc = "Updated taxon from " + oldTaxonName + "(" + oldTaxonId + ")" + " to " + newTaxonName
-					+ "(" + updatedSpecies.getTaxonConceptId() + ")";
+			String activityDesc = "Updated taxon from " + oldTaxonName + "(Taxon ID:" + oldTaxonId + ")" + " to " + newTaxonName
+					+ "(Taxon ID:" + updatedSpecies.getTaxonConceptId() + ")";
 
 			// Log activity for taxon ID update
 			logActivity.LogActivity(request.getHeader(HttpHeaders.AUTHORIZATION), activityDesc, speciesId, speciesId,
