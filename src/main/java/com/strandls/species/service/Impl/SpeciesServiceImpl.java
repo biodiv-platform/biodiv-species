82771483bf788a865d3b62a3e003712417678228
mekala.ravi <mekala.ravi@strandls.com>
2026-04-14 11:39:33 +0530

    Removed logs

--- src/main/java/com/strandls/species/service/Impl/SpeciesServiceImpl.java
+++ src/main/java/com/strandls/species/service/Impl/SpeciesServiceImpl.java
@@ -390,8 +390,6 @@ public class SpeciesServiceImpl implements SpeciesServices {
 
 		for (Field field : obj.getClass().getDeclaredFields()) {
 			if (Modifier.isStatic(field.getModifiers())) {
-				logger.info("Skipping static field: {} in class: {}",
-					field.getName(), obj.getClass().getName());
 				continue;
         	}
 			field.setAccessible(true);
